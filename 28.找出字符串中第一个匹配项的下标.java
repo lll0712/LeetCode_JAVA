3477f6063c8e72220279893e70a92e23e048e429
OVO <1754690284@qq.com>
2024-09-15 09:57:48 +0800

    28

---
+++ 28.找出字符串中第一个匹配项的下标.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=java
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ */
+
+// @lc code=start
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+    }
+}
+// @lc code=end
+
